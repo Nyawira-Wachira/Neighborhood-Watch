5bb2c7f2549041b612fa2875ba5c4e9f26b3b579
ABBY <abbyW@moringa.com>
2022-06-19 16:31:20 +0300

    Created User register form, view, url path and index template for authenticate templates.

--- watch/views.py
+++ watch/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
+
+def Register(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
+    else:
+        form = CreateUserForm()
+        if request.method =='POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user=form.cleaned_data.get('username')
+                messages.success(request, "Account Created Successfully! You are now able to log in")
+
+                return redirect('login')
+
+
+        return render(request, 'authenticate/register.html',{'form':form} )
