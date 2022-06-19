20e0357d1893491fc14a4b17d604aa45acd125f5
ABBY <abbyW@moringa.com>
2022-06-19 20:41:46 +0300

    Created function for user profile update.

--- watch/views.py
+++ watch/views.py
@@ -4,6 +4,7 @@ from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from .forms import UserUpdateForm, ProfileUpdateForm
 from .models import Profile
 # Create your views here.
 
@@ -54,3 +55,26 @@ def UserProfile(request):
 
 
     return render(request, 'profile.html')
+
+@login_required
+def update(request):
+    if request.method == 'POST':
+            u_form = UserUpdateForm(request.POST, instance=request.user)
+            p_form = ProfileUpdateForm(request.POST,
+                                    request.FILES,
+                                    instance=request.user.profile)
+            if u_form.is_valid() and p_form.is_valid():
+                u_form.save()
+                p_form.save()
+                messages.success(request, f'Your account has been updated successfully!')
+                return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'update_profile.html', context)
