ba778a727d6e563e25ffc86122816be011494da9
ABBY <abbyW@moringa.com>
2022-06-19 18:46:49 +0300

    created login function, url path and login template.

--- watch/urls.py
+++ watch/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns=[
     path('register',views.Register, name='register'),
+    path('login/',views.Login, name='login'), 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- watch/views.py
+++ watch/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,redirect
 from .forms import CreateUserForm
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login, logout
 # Create your views here.
 
 def Register(request):
@@ -20,3 +21,22 @@ def Register(request):
 
 
         return render(request, 'authenticate/register.html',{'form':form} )
+
+def Login(request):
+    if request.user.is_authenticated:
+        return redirect('profile')
+    else:
+        if request.method =='POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+
+            user = authenticate(request, username=username, password=password)
+
+            if user is not None:
+                login(request, user)
+                return redirect('profile')
+
+            else:
+                messages.error (request, 'Check username or password and try again')
+
+        return render(request, 'authenticate/login.html' )
